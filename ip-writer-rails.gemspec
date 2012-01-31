dcb569591f0df6ce6cb44a3315fca124d510d7de
Jonathan Vingiano <jvingiano@gmail.com>
2012-01-31 13:59:02 -0500

    v1

--- ip-writer-rails.gemspec
+++ ip-writer-rails.gemspec
@@ -1,6 +1,9 @@
+# -*- encoding: utf-8 -*-
+require File.expand_path('../lib/ip-writer-rails/version', __FILE__)
+
 Gem::Specification.new do |s|
   s.name = "ip-writer-rails"
-  s.version = "0.1"
+  s.version = IPWriter::VERSION
   s.authors     = ["Jonathan Vingiano"]
   s.email       = ["jgv@jonathanvingiano.com"]
   s.homepage    = "http://github.com/jgv/ip-writer-rails"
@@ -9,9 +12,10 @@ Gem::Specification.new do |s|
 
   s.summary = "Ruby fork of Kim Asendorf's IP Writer"
   s.description = "Read the IP Address of a visitor to your website and tweet it."
-  s.files = Dir["lib/**/*"] + + ["MIT-LICENSE", "Rakefile", "README.textile"]
+  s.files = Dir["lib/*"] + ["MIT-LICENSE", "Rakefile", "README.textile"]
   
-  s.add_dependency('twitter')
+  s.add_dependency "rails", "~> 3.0"
+  s.add_dependency "twitter"
   
   s.require_paths = ['lib']
 end
--- lib/ip-writer-rails.rb
+++ lib/ip-writer-rails.rb
@@ -1,27 +1,23 @@
 require "ip-writer-rails/version"
+require "twitter"
 
 module IPWriter
-define("CONSUMER_KEY", "4IIcFWiOs5JQi4vI2umTTw");
-define("CONSUMER_SECRET", "Cc9bjleqldFfnSDB1nPaMDABjsTseYwq1cLPAnnk");
-define("OAUTH_TOKEN", "45836347-39kiTx3oYBz1dtRUHQTFxrdVPj1NYPH2AO1XcPkfh");
-define("OAUTH_SECRET", "SoIXUYgC1Tl06xoUpVflzdR9ERvkV4PMuWKQFJupqk");
 
-  def ip_writer(consumer_key, consumer_secret, oauth_token, oauth_secret)    
-    if !arguments || arguments.legnth != 4
-      Twitter.configure do |config|
-        config.consumer_key = '4IIcFWiOs5JQi4vI2umTTw'
-        config.consumer_secret = 'Cc9bjleqldFfnSDB1nPaMDABjsTseYwq1cLPAnnk'
-        config.oauth_token = '45836347-39kiTx3oYBz1dtRUHQTFxrdVPj1NYPH2AO1XcPkfh'
-        config.oauth_token_secret = 'SoIXUYgC1Tl06xoUpVflzdR9ERvkV4PMuWKQFJupqk'
-      end
-    else
-      Twitter.configure do |config|
-        config.consumer_key = consumer_key
-        config.consumer_secret = consumer_secret
-        config.oauth_token = oauth_token
-        config.oauth_token_secret = oauth_secret
-      end      
+  def ip_writer(consumer_key='4IIcFWiOs5JQi4vI2umTTw', consumer_secret='Cc9bjleqldFfnSDB1nPaMDABjsTseYwq1cLPAnnk', oauth_token='45836347-39kiTx3oYBz1dtRUHQTFxrdVPj1NYPH2AO1XcPkfh', oauth_secret='SoIXUYgC1Tl06xoUpVflzdR9ERvkV4PMuWKQFJupqk')
+    
+    Twitter.configure do |config|
+      config.consumer_key = consumer_key
+      config.consumer_secret = consumer_secret
+      config.oauth_token = oauth_token
+      config.oauth_token_secret = oauth_secret
     end
-    Twitter.update(request.remote_ip)
+    begin
+      Twitter.update(request.remote_ip)
+    rescue Exception => e
+      puts "Error: #{e}"
+    end
   end
+
 end
+
+ActionController::Base.send :include, IPWriter
